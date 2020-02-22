014c6fcbf9f2c9fbd61287d1d39a0c375aede0b2
woairong <34139640+woairong@users.noreply.github.com>
2020-02-22 18:51:53 +0800

    Add files via upload

--- 数据预处理/数据选择.py
+++ 数据预处理/数据选择.py
@@ -1,1 +1,13 @@
+import pandas as pd
+
+df1 = pd.read_csv('G:\\ml360\\train\\traindata1.txt', sep='\t')
+df2 = pd.read_csv('G:\\ml360\\train\\traindata2.txt', sep='\t')
+df3 = pd.read_csv('G:\\ml360\\train\\traindata3.txt', sep='\t')
+df4 = pd.read_csv('G:\\ml360\\train\\traindata4.txt', sep='\t')
+df5 = pd.read_csv('G:\\ml360\\train\\traindata5.txt', sep='\t')
+df1.to_csv('G:\\ml360\\train\\1104.csv', index = False, sep = ',')
+df20 = df2.ix[0:13463,:]
+df1.to_csv('G:\\ml360\\train\\1104.csv', index = False, sep = ',')
+df20.to_csv('G:\\ml360\\train\\1104.csv', mode = 'a', index = False, header = False, sep = ',')
+df1104 = pd.read_csv('G:\\ml360\\train\\1104.csv')
 
